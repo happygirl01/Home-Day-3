d22d7c606f679a12b97ad153dbf3afaf34753763
labake <labakelemboye@gmail.com>
2016-11-30 19:04:52 +0100

    update

--- tests/binarysearch.js
+++ tests/binarysearch.js
@@ -1,14 +1,4 @@
-//Firstly you are to create three functions, as prototypes to the Array class to return the following:
-//toTwenty()` returns `[1, 2, 3 . . . 20]
-//toForty() returns [2, 4, 6 . . . 40]
-//toOneThousand() returns [10, 20, 30 . . . 1000]
-//Once you are done, create another prototype(d) function called search, it will take just one argument which is the number you are to find. 
-//The search function should return an object, which contains
-//1.count, the number of times you function iterated to find the index of the number in question
-//2.index, the index of the number in question
-//3.length, the length of the original array
-//The .search function should implement the binary search algorithm, each time you iterate, you should increase the count, to test how 
-//efficient your implementation is.
+
 'use strict';
 var binarySearch = require('../app/binarysearch.js');
 (function(){
